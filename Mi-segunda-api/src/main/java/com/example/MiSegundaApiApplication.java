f1d019d4da4038c2380166a9c46acab0d8665d02
Jdiazz16 <160591075+JenniferDiazz166@users.noreply.github.com>
2024-07-01 13:42:22 +0200

    Add files via upload

---
+++ Mi-segunda-api/src/main/java/com/example/MiSegundaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiSegundaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiSegundaApiApplication.class, args);
+	}
+
+}
---
+++ Mi-segunda-api/src/main/java/com/example/springbootapi/controller/HelloController.java
@@ -0,0 +1,9 @@
+package com.example.springbootapi.controller;
+
+
+public class HelloController {
+	public String sayHello() {
+        return "Hello, World!";
+
+}
+}
